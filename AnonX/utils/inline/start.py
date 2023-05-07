8f3996cb53ac70a8f100cd835454b9aae3de33b7
toxicop77 <107669917+toxicop77@users.noreply.github.com>
2023-05-07 10:39:13 +0530

    Update start.py
    Signed-off-by: toxicop77 <107669917+toxicop77@users.noreply.github.com>

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -49,7 +49,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="✨ sᴏᴜʀᴄᴇ ✨", url=config.UPSTREAM_REPO
+                text="✨ sᴏᴜʀᴄᴇ ✨", url=https://graph.org/file/b4d2c0fffe70b1c560b72.jpg
             )
         ],
      ]
