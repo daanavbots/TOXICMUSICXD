fa889e76fd85bf42efc5d5b8dd855310c366f87e
toxicop77 <107669917+toxicop77@users.noreply.github.com>
2023-05-09 10:33:18 +0530

    Update config.py
    Signed-off-by: toxicop77 <107669917+toxicop77@users.noreply.github.com>

--- config.py
+++ config.py
@@ -105,6 +105,8 @@ SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
 
 SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
 
+SOURCE_PIC = "https://graph.org/file/b4d2c0fffe70b1c560b72.jpg"
+
 
 def time_to_seconds(time):
     stringt = str(time)
